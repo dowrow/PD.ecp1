cdd375b1a9a95b25fe7b2f327187dcd69857b433
Diego Castaño Chillarón <dowrow@outlook.com>
2015-10-10 09:42:42 +0200

    Created and updated ReferencesFactory.

---
+++ src/main/java/es/upm/miw/pd/ecp1/singleton/factory/ReferencesFactory.java
@@ -0,0 +1,37 @@
+package es.upm.miw.pd.ecp1.singleton.factory;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ReferencesFactory {
+    
+    private static ReferencesFactory instance = new ReferencesFactory();
+    
+    private Map<String, Integer> references;
+
+    private int reference;
+
+    private ReferencesFactory() {
+        this.references = new HashMap<>();
+        this.reference = 0;
+    }
+
+    public static ReferencesFactory getFactory() {
+        return instance;
+    }
+    
+    public int getReference(String key) {
+        Integer result = this.references.get(key);
+        if (result == null) {
+            this.references.put(key, this.reference);
+            result = this.reference;
+            reference++;
+        }
+        return result;
+    }
+
+    public void removeReference(String key) {
+        this.references.remove(key);
+    }
+
+}
