4fe84d3afa763a5af0ff70e89c3625918f7e2984
Diego Castaño Chillarón <diego.castano@alumnos.upm.es>
2015-10-14 22:12:08 +0200

    Created visitor.figure classes. Deleted example classes from package.

--- src/main/java/es/upm/miw/pd/ecp1/visitor/figure/Element.java
+++
@@ -1,5 +0,0 @@
-package es.upm.miw.pd.ecp1.visitor.figure;
-
-public interface Element {
-    void accept(Visitor v);
-}
---
+++ src/main/java/es/upm/miw/pd/ecp1/visitor/figure/Figure.java
@@ -0,0 +1,11 @@
+package es.upm.miw.pd.ecp1.visitor.figure;
+
+public abstract class Figure {
+
+    public Figure(String description) {
+    }
+    
+    public abstract double area();
+    public abstract double numberOfSides();
+
+}
